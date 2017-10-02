4ac02dcf21072ffdb589ec622a0d0c7fae1f069d
Alex Faber <alex.faber@singlewire.com>
2017-10-01 19:07:07 -0700

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const autoprefixer = require('gulp-autoprefixer');
+
+const input = './stylesheets/**/*.scss';
+const output = './public/css';
+const sassOptions = {
+  errLogToConsole: true,
+  outputStyle: 'expanded',
+}
+
+gulp.task('sass', function () {
+  return gulp
+    .src(input)
+    .pipe(sourcemaps.init())
+    .pipe(sass(sassOptions).on('error', sass.logError))
+    .pipe(sourcemaps.write())
+    .pipe(autoprefixer())
+    .pipe(gulp.dest(output));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('stylesheets/**/*.scss', ['sass']);
+});
